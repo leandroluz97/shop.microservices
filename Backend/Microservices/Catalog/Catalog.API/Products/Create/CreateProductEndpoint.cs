18b6a0882a0cd34b45571065bb610354008f33ef
leandroluz97 <leandrodaluz97@gmail.com>
2024-10-19 14:48:03 +0100

    implement CreateProductCommandHandler from common CQRS  interface

--- Backend/Microservices/Catalog/Catalog.API/Products/Create/CreateProductEndpoint.cs
+++ Backend/Microservices/Catalog/Catalog.API/Products/Create/CreateProductEndpoint.cs
@@ -1,5 +1,15 @@
-﻿namespace Catalog.API.Products.Create
+﻿using Common.CQRS;
+
+namespace Catalog.API.Products.Create
 {
+    public record CreateProductRequest(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price);
+
+    public record CreatProductResponse(Guid Id);
     public class CreateProductEndpoint
     {
     }
--- Backend/Microservices/Catalog/Catalog.API/Products/Create/CreateProductHandler.cs
+++ Backend/Microservices/Catalog/Catalog.API/Products/Create/CreateProductHandler.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using Catalog.API.Models;
+using Common.CQRS;
+using MediatR;
 
 namespace Catalog.API.Products.Create
 {
@@ -7,14 +9,23 @@ namespace Catalog.API.Products.Create
         List<string> Category, 
         string Description, 
         string ImageFile, 
-        decimal Price) : IRequest<CreatProductResult>;
+        decimal Price) : ICommand<CreatProductResult>;
 
     public record CreatProductResult(Guid Id);
-    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreatProductResult>
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreatProductResult>
     {
-        public Task<CreatProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreatProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price,
+            };
+
+            return new CreatProductResult(Guid.NewGuid());  
         }
     }
 }
