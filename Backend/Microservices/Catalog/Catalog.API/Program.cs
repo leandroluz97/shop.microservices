5c8739344aa1e723a4adaf8c0ec044a2e46bb4eb
leandroluz97 <leandrodaluz97@gmail.com>
2024-10-23 09:06:17 +0100

    added using global

--- Backend/Microservices/Catalog/Catalog.API/Program.cs
+++ Backend/Microservices/Catalog/Catalog.API/Program.cs
@@ -5,6 +5,10 @@ builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
 });
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
