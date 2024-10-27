470ddc42f5ed8e94b996c3ae27750bbfac8457f0
leandroluz97 <leandrodaluz97@gmail.com>
2024-10-27 12:07:54 +0000

    added GetProducts endpoints

---
+++ Backend/Microservices/Catalog/Catalog.API/Products/GetAll/GetProductsEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+using Catalog.API.Products.Create;
+
+namespace Catalog.API.Products.GetAll
+{
+
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+             .WithName("GetProducts")
+             .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status404NotFound)
+             .WithSummary("Get Products")
+             .WithDescription("Get all Products");
+        }
+    }
+}
