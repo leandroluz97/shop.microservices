b0ad759f6693f405fcd32a3631caadf48262c0ce
leandroluz97 <leandrodaluz97@gmail.com>
2024-10-28 08:16:38 +0000

    added get product by Id handler

---
+++ Backend/Microservices/Catalog/Catalog.API/Products/GetById/GetProductByIdEndpoint.cs
@@ -0,0 +1,27 @@
+﻿namespace Catalog.API.Products.GetById
+{
+    public record GetProductResponse(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price);
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var command = new GetProductByIdQuery(id) ;
+                var result = await sender.Send(command);
+                var response = result.Adapt<GetProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById ")
+            .Produces<GetProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
---
+++ Backend/Microservices/Catalog/Catalog.API/Products/GetById/GetProductByIdHandler.cs
@@ -0,0 +1,35 @@
+﻿using Catalog.API.Exceptions;
+using Catalog.API.Products.GetAll;
+
+namespace Catalog.API.Products.GetById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(
+        string Name,
+        List<string> Category,
+        string Description,
+        string ImageFile,
+        decimal Price);
+    public class GetProductByIdHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if(product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(
+                product.Name, 
+                product.Category, 
+                product.Description, 
+                product.ImageFile, 
+                product.Price);
+        }
+    }
+}
