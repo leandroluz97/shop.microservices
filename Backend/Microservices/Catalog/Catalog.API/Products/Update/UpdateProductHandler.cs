0abc508a260cfcdbe022a943cad169f2aa5ffb17
leandroluz97 <leandrodaluz97@gmail.com>
2024-10-30 21:13:07 +0000

    added update product

---
+++ Backend/Microservices/Catalog/Catalog.API/Products/Update/UpdateProductHandler.cs
@@ -0,0 +1,45 @@
+﻿
+using Catalog.API.Exceptions;
+
+namespace Catalog.API.Products.Update
+{
+    public record UpdateProductCommand(
+       Guid Id,
+       string Name,
+       List<string> Category,
+       string Description,
+       string ImageFile,
+       decimal Price) : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(
+        Guid Id,
+       string Name,
+       List<string> Category,
+       string Description,
+       string ImageFile,
+       decimal Price);
+    public class UpdateProductHandler(IDocumentSession session) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken) ?? throw new ProductNotFoundException();
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(
+                product.Id,
+                product.Name,
+                product.Category,
+                product.Description,
+                product.ImageFile,
+                product.Price);
+        }
+    }
+}
