b2ebe8726c5c890da0963e990ffc882f3b13a644
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-01 18:56:23 +0000

    added delete and enpoint

---
+++ Backend/Microservices/Catalog/Catalog.API/Products/Delete/DeleteProductEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Catalog.API.Products.Create;
+using Catalog.API.Products.Update;
+
+namespace Catalog.API.Products.Delete
+{
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+                return Results.Ok();
+            })
+             .WithName("DeleteProduct")
+             .Produces<CreateProductRequest>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .ProducesProblem(StatusCodes.Status404NotFound)
+             .WithSummary("Delete Product")
+             .WithDescription("Delete Product");
+        }
+    }
+}
