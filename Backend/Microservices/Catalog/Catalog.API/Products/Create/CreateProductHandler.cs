db434d5219f4c092d4707487257a6067c9c3bebf
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-04 22:50:12 +0000

    added CreateProductCommandValidator to validate create command DTO

--- Backend/Microservices/Catalog/Catalog.API/Products/Create/CreateProductHandler.cs
+++ Backend/Microservices/Catalog/Catalog.API/Products/Create/CreateProductHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Catalog.API.Products.Create
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.Create
 {
     public record CreateProductCommand(
         string Name, 
@@ -8,6 +10,17 @@
         decimal Price) : ICommand<CreatProductResult>;
 
     public record CreatProductResult(Guid Id);
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price is required");
+        }
+    }
     public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreatProductResult>
     {
 
--- Backend/Microservices/Catalog/Catalog.API/Program.cs
+++ Backend/Microservices/Catalog/Catalog.API/Program.cs
@@ -1,10 +1,15 @@
+using Common.Behaviors;
+using FluentValidation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
 });
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(config =>
 {
     config.Connection(builder.Configuration.GetConnectionString("Database")!);
