09c1504deebbc87eb2b15267b78aaaed24744bcc
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-06 07:33:12 +0000

    added UpdateProductCommandValidator to validate  UpdateProductCommand DTO

--- Backend/Microservices/Catalog/Catalog.API/Products/Update/UpdateProductHandler.cs
+++ Backend/Microservices/Catalog/Catalog.API/Products/Update/UpdateProductHandler.cs
@@ -1,6 +1,8 @@
 ﻿
 using Catalog.API.Exceptions;
+using Catalog.API.Products.Create;
 using Catalog.API.Products.GetAll;
+using FluentValidation;
 
 namespace Catalog.API.Products.Update
 {
@@ -19,6 +21,18 @@ namespace Catalog.API.Products.Update
        string Description,
        string ImageFile,
        decimal Price);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("ProductID is required");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price is required");
+        }
+    }
     public class UpdateProductHandler(IDocumentSession session, ILogger<UpdateProductHandler> logger) : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
