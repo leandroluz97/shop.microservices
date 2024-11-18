5c8f46f24cfdd9d530a54cab1dc66d001c06bd09
leandroluz97 <leandrodaluz97@gmail.com>
2024-11-18 08:57:59 +0000

    added Basket microservice setup

---
+++ Backend/Microservices/Basket/Basket.API/Basket/Delete/DeleteBasketHandler.cs
@@ -0,0 +1,6 @@
+﻿namespace Basket.API.Basket.Delete
+{
+    public class DeleteBasketHandler
+    {
+    }
+}
---
+++ Backend/Microservices/Basket/Basket.API/Model/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace Basket.API.Model
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = [];
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public ShoppingCart()
+        {
+            
+        }
+
+    }
+}
