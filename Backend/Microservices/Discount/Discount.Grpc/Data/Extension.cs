0ecb1fc182043376610ff67164db15665a790797
leandroluz97 <leandrodaluz97@gmail.com>
2024-12-14 15:45:14 +0000

    added IApplicationBuilder extensions to migrate migrations

---
+++ Backend/Microservices/Discount/Discount.Grpc/Data/Extension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public static class Extension
+    {
+
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app) 
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dBcontext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+            dBcontext.Database.MigrateAsync();
+            return app;
+        }
+    }
+}
