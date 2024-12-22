66ce5f1ab989a8e474cfa87610380938037615e0
leandroluz97 <leandrodaluz97@gmail.com>
2024-12-22 00:42:23 +0000

    added Ordering API dependency injection from Application and Infrastructure layers

--- Backend/Microservices/Ordering/Ordering.API/Program.cs
+++ Backend/Microservices/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,15 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices(builder.Configuration);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
