0358a134daf7ad466bf9d3876cc102b95e10818b
leandroluz97 <leandrodaluz97@gmail.com>
2024-12-22 00:40:28 +0000

    added Ordering Infrastructure Dependency Injection

---
+++ Backend/Microservices/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            //services.AddDbContext<ApplicationDbContext>((options) =>
+            //{
+            //    var connectionString = configuration.GetConnectionString("Database")!;
+            //    options.UseSqlServer(connectionString);
+            //});
+
+            //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+            return services;
+        }
+    }
+}
