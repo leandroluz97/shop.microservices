a1237134ed68da6542a9e40f3f7e9f860df42d3d
leandroluz97 <leandrodaluz97@gmail.com>
2025-01-14 22:33:04 +0000

    added Delete Order Command and Handler

---
+++ Backend/Microservices/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public record DeleteOrderCommand(Guid Id) :  ICommand<DeleteOrderResult>;
+    public record DeleteOrderResult(bool IsSuccess);
+
+    public class UpdateOrderCommandValidator : AbstractValidator<DeleteOrderCommand>
+    {
+        public UpdateOrderCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("OrderId is required");
+        }
+    }
+
+}
---
+++ Backend/Microservices/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IApplicationDbContext dbContext) : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var orderId = OrderId.Of(command.Id);
+            var order = await dbContext.Orders.FindAsync([orderId], cancellationToken);
+
+            if (order is null)
+            {
+                throw new OrderNotFoundException(orderId.Value);
+            }
+
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteOrderResult(true);
+        }
+    }
+}
