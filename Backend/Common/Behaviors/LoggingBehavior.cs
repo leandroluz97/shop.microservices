9d9e3b6947998366f6161c547c854c259e437778
leandroluz97 <leandrodaluz97@gmail.com>
2025-01-16 01:15:05 +0000

    updated LoggingBehavior

--- Backend/Common/Behaviors/LoggingBehavior.cs
+++ Backend/Common/Behaviors/LoggingBehavior.cs
@@ -24,7 +24,7 @@ namespace Common.Behaviors
             if(timeTaken.Seconds > 3)
             {
                 logger.LogInformation("[PERFORMANCE] the request {Request} took {Timetaken} seconds",
-                typeof(TRequest).Name, typeof(TResponse).Name);
+                typeof(TRequest).Name, timeTaken.Seconds);
             }
 
             logger.LogInformation("[END] Handle {Request} with {Response}",
