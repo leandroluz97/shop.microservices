1d6feb3afd0a1e64ec6dde210d8e54148291250d
leandroluz97 <leandrodaluz97@gmail.com>
2025-01-16 01:14:37 +0000

    added Order Events Handlers

---
+++ Backend/Microservices/Ordering/Ordering.Application/Orders/EventsHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Ordering.Application.Orders.EventsHandlers
+{
+    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger) :
+        INotificationHandler<OrderCreatedEvent>
+    {
+        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event Handled: {DomainEvent}", notification.GetType());
+            return Task.CompletedTask;
+        }
+    }
+}
