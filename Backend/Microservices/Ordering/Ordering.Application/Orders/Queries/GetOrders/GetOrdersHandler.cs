8e5a7e6f136587a0d0631dc2065462701b53e0aa
leandroluz97 <leandrodaluz97@gmail.com>
2025-01-23 23:15:23 +0000

    added GetOrders Handler

---
+++ Backend/Microservices/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public class GetOrdersHandler(IApplicationDbContext dbContext) : IQueryHandler<GetOrdersQuery, GetOrdersResult>
+    {
+        public async Task<GetOrdersResult> Handle(GetOrdersQuery query, CancellationToken cancellationToken)
+        {
+            var pageNumber = query.PaginationOptions.PageNumber;
+            var pageSize = query.PaginationOptions.PageSize;
+
+            var ordersCount = await dbContext.Orders.LongCountAsync(cancellationToken);
+
+            var orders = await dbContext.Orders
+                .Include(o => o.OrderItems)
+                .AsNoTracking()
+                .OrderBy(o => o.OrderName.Value)
+                .Skip(pageSize * pageNumber)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            var result = new PaginationResult<OrderDto>(pageNumber, pageSize, ordersCount, orders.ToOrderDtoList());
+            return new GetOrdersResult(result);
+        }
+    }
+}
---
+++ Backend/Microservices/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,8 @@
+﻿using Common.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public record GetOrdersQuery(PaginationOptions PaginationOptions) : IQuery<GetOrdersResult>;
+    public record GetOrdersResult(PaginationResult<OrderDto> Orders);
+
+}
