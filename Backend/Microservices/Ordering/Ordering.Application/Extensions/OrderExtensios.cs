6f627d20e957b891772a69991707d13fb0ec4d61
leandroluz97 <leandrodaluz97@gmail.com>
2025-01-23 23:14:35 +0000

    added order extension

---
+++ Backend/Microservices/Ordering/Ordering.Application/Extensions/OrderExtensios.cs
@@ -0,0 +1,45 @@
+﻿namespace Ordering.Application.Extensions
+{
+    public static class OrderExtensios
+    {
+        public static IEnumerable<OrderDto> ToOrderDtoList(this IEnumerable<Order> orders)
+        {
+            List<OrderDto> ordersDtos = [];
+            foreach (var order in orders)
+            {
+                var orderDto = new OrderDto(
+                    Id: order.Id.Value,
+                    CustomerId: order.CustomerId.Value,
+                    OrderName: order.OrderName.Value,
+                    ShippingAddress: new AddressDto(
+                        order.ShippingAddress.FirstName,
+                        order.ShippingAddress.LastName,
+                        order.ShippingAddress.EmailAddress,
+                        order.ShippingAddress.AddressLine,
+                        order.ShippingAddress.Country,
+                        order.ShippingAddress.State,
+                        order.ShippingAddress.ZipCode),
+                     BillingAddress: new AddressDto(
+                        order.ShippingAddress.FirstName,
+                        order.ShippingAddress.LastName,
+                        order.ShippingAddress.EmailAddress,
+                        order.ShippingAddress.AddressLine,
+                        order.ShippingAddress.Country,
+                        order.ShippingAddress.State,
+                        order.ShippingAddress.ZipCode),
+                     Payment: new PaymentDto(
+                         order.Payment.CardName,
+                         order.Payment.CardNumber,
+                         order.Payment.Expiration,
+                         order.Payment.CVV,
+                         order.Payment.PaymentMethod),
+                     Status: order.Status,
+                     OrderItems: order.OrderItems.Select(x => new OrderItemDto(x.ProductId.Value, order.CustomerId.Value, x.Quantity, x.Price)).ToList());
+
+                ordersDtos.Add(orderDto);
+            }
+
+            return ordersDtos;
+        }
+    }
+}
