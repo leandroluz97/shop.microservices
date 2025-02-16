01314258fbeaa71ad0f13f8f5d802cc926cc511d
leandroluz97 <leandrodaluz97@gmail.com>
2025-02-16 16:58:27 +0000

    added Common Messaging Entities

---
+++ Backend/Common.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Common.Messaging.MassTransit
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddMessageBroker(this IServiceCollection services, IConfiguration configuration, Assembly? assembly = null )
+        {
+            services.AddMassTransit((builder) =>
+            {
+                builder.SetKebabCaseEndpointNameFormatter();
+
+                if (assembly is not null)
+                    builder.AddConsumers(assembly);
+
+                builder.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(new Uri(configuration["MessageBroker:Host"]!), host =>
+                    {
+                        host.Username(configuration["MessageBroker:UserName"]!);
+                        host.Password(configuration["MessageBroker:Password"]!);
+                    });
+                    configurator.ConfigureEndpoints(context);
+                });
+
+            });
+            return services;
+        }
+    }
+}
